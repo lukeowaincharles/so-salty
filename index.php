6f404e2f5ccf28f4f290e86e823704b311c43cee
unknown <lukeowaincharles@gmail.com>
2018-02-25 14:00:28 +0000

    barebones layout for homepage

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+        <title>So Salty shop</title>
+        <meta name="description" content="">
+        <link rel="stylesheet" href="/static/ext/bootstrap/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/static/ext/bootstrap/css/bootstrap.min.css">
         
     </head>
     <body>
+        <section class="hero__gradient">
+            <div class="logo__wrapper">
+                <img src="/static/img/logo-so-salty.svg" />
+            </div>
+            <p class="page__scroll" id="">Scroll</p>
+        </section>
         
+        <main>
+            <section class="product__wrapper">
+                <div class="product__image">
+                    <img src="/static/img/poster-mockup.png" alt="So Salty Poster" />
+                </div>
+                <div class="product__info__wrapper">
+                    <h5>Limited edition</h5>
+                    <h1>So Salty Poster</h1>
+                    <p class="product__copy">Bright and vibrant "So Salty" A2 poster. Lighten up from being Salty (the act of being upset, angry or bitter) with this super fun poster.</p>
+                    <p class="product__price">£12.99 <span>plus P&amp;P</span>
+                    </p>
+                    <button>Buy now</button>
+                </div>
+            </section>
+        </main>
+        
+        <footer>
+            <nav>
+                <ul>
+                    <li><a href="#">Contact</a></li>
+                    <li class="seperator"></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+        </footer>
     </body>
 </html>
